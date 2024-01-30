bf30193bafdddf0b492fe616d900598c25155a64
Eduardo Barra Balao <ebarrab@gmail.com>
2024-01-30 22:46:31 +0100

    0.1 Ejemplo FPTClientExample. Descargar de fichero ZIP y fichero de texto. Modos binario y ASCII

---
+++ src/ClienteFTP.java
@@ -0,0 +1,9 @@
+import ejemplos.FTPClientExample;
+
+import java.net.UnknownHostException;
+
+public class ClienteFTP {
+    public static void main(String[] args) {
+        System.out.println("ClienteFTP 0.1");
+    }
+}
